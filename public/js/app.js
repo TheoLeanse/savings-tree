dd277cc3d50798efd4a8ccaac43e666d998e7019
Kirsten Jones <kirstenjones@yahoo.co.uk>
2015-09-19 15:22:07 +0200

    time until deadline is displayed

--- public/js/app.js
+++ public/js/app.js
@@ -4,13 +4,21 @@ app.controller("SavingsTreeController", function() {
     var self = this;
     self.target = 0;
     self.timeframe = 0;
+    self.startdate = 0;
+    self.enddate = moment();
 
     self.addTarget = function(target, timeframe) {
         self.target = target;
         self.timeframe = timeframe;
+        self.startdate = moment();
+        self.enddate = moment().add(timeframe, 'minutes');
     };
 
     self.hasTarget = function() {
         return self.target > 0 && self.timeframe > 0;
+    };
+
+    self.timeLeft = function() {
+      return self.enddate.diff(moment(), 'minutes');
     };
 });
