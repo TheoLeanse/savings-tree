d8a5c147263cda685acfe7cf2695dca5b42b0db5
tommasobratto <tbratto22@gmail.com>
2015-09-19 13:17:09 +0200

    added setting time span feature

--- public/js/app.js
+++ public/js/app.js
@@ -3,12 +3,14 @@ var app = angular.module("SavingsTree", []);
 app.controller("SavingsTreeController", function() {
     var self = this;
     self.target = 0;
+    self.timeframe = 0;
 
-    self.addTarget = function(target) {
+    self.addTarget = function(target, timeframe) {
         self.target = target;
+        self.timeframe = timeframe;
     };
 
     self.hasTarget = function() {
-        return self.target > 0;
+        return self.target > 0 && self.timeframe > 0;
     };
 });
--- test/e2e/targetFeatureSpec.js
+++ test/e2e/targetFeatureSpec.js
@@ -5,22 +5,28 @@ describe("Setting a target", function() {
 
     var targetAmount = element(by.className("target-amount")),
         targetField  = element(by.className("target-field")),
-        addTargetBtn = element(by.className("add-target"));
+        addTargetBtn = element(by.className("add-target")),
+        timeField = element(by.className("time-field")),
+        timeSpan = element(by.className("time-span"));
 
     it("should let a user set their target", function() {
         targetField.sendKeys("1000");
+        timeField.sendKeys("10");
         addTargetBtn.click();
         expect(targetAmount.getText()).toContain("1000");
+        expect(timeSpan.getText()).toContain("10");
     });
 
-    it("should hide the target if no there is no target set", function() {
+    it("should hide the target information if there is no target set", function() {
         expect(targetAmount.isDisplayed()).toBeFalsy();
+        expect(timeSpan.isDisplayed()).toBeFalsy();
     });
-
 
     it("should hide the form once the user set their target", function() {
         targetField.sendKeys("1000");
+        timeField.sendKeys("10");
         addTargetBtn.click();
         expect(targetField.isDisplayed()).toBeFalsy();
+        expect(timeField.isDisplayed()).toBeFalsy();
     });
 });
